5a906e99b3b805ebf2a4fc80c15ac62cdbd796b9
Violeta Pugliese <violeta.pugliese@gmail.com>
2021-05-28 21:51:58 -0300

    conexión y primer transacción

---
+++ src/index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+// SDK de Mercado Pago
+const mercadopago = require ('mercadopago');
+
+//middleware
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// Agrega credenciales
+mercadopago.configure({
+    access_token: 'APP_USR-6623451607855904-111502-1f258ab308efb0fb26345a2912a3cfa5-672708410'
+  });
+
+//routes
+app.post('/checkout', (req, res) => {
+// Crea un objeto de preferencia
+
+let preference = {
+    items: [
+      {
+        title:req.body.title,
+        unit_price: parseInt(req.body.price),
+        quantity: 1,
+      }
+    ]
+  };
+  
+  mercadopago.preferences.create(preference)
+  .then(function(response){
+  
+    res.redirect(response.body.init_point);
+   
+  }).catch(function(error){
+    console.log(error);
+  });
+});
+
+//server
+
+app.listen(3000, () => {
+    console.log("Server on port 3000");
+});
